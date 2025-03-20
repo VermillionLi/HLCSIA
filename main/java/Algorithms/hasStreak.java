c517dab16a035d8db196401631226c63bce7d691
Juntong Li <147962124+VermillionLi@users.noreply.github.com>
2025-03-19 23:20:43 -0700

    Delete main directory

--- main/java/Algorithms/hasStreak.java
+++
@@ -1,9 +0,0 @@
-package Algorithms;
-
-public interface hasStreak {
-    //minStreak should be changable according to client's settings in the database
-    int minLoseStreak = 2;
-    int minWinStreak = 3;
-public void findStreak();
-}
-
